cf366781b2925833e27722728753ca2d43ed7b22
Ebene <ebeneoranje60@gmail.com>
2023-04-21 00:16:32 +0200

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - A function that puts a string
+ *Return: zero
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
