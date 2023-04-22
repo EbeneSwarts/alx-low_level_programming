d7d49be887fc434735d3ad06cbf75ff204d55b40
Ebene <ebeneoranje60@gmail.com>
2023-04-22 20:36:30 +0200

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,24 +6,24 @@
 /* main - identify random number as positive or negative*/
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
+srand(time(0));
 
-    n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
     if (n > 0)
     {
-        printf("%d is positive\n",n);
+    printf("%d is positive\n", n);
     }
-	else if (n == 0)
-	{
-        printf("%d is zero\n",n);
+    else if (n == 0)
+    {
+    printf("%d is zero\n", n);
     }
 	else
     {
-        printf("%d is negative\n",n);
+    printf("%d is negative\n", n);
     }
 
-	return (0);
+    return (0);
 }
