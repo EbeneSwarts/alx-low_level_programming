2036ecaebf235efed997f9ce2115541ceccf1ea6
Ebene <ebeneoranje60@gmail.com>
2023-04-25 21:50:00 +0200

    Low level programming files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main: prints _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+int _putchar(char )
+{
+	putchar('\n')
+
+    return (0);
+}
