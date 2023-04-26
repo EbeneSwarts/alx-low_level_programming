876dcaf8f4ee97c031971bd0ab1f1b5072031e2a
Ebene <ebeneoranje60@gmail.com>
2023-04-27 00:52:16 +0200

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,9 +6,9 @@
  * Description: This function prints the last digit of a number
  * Return: last digit of number r
  */
-int print_last_digit (int r)
+int print_last_digit(int r)
 {
-	int n;
+    int n;
 
 	if (r < 0)
 		n = -1 * (r % 10);
