304f8c3ead700011360498b18f8dd38a50b1c6e3
Ebene <ebeneoranje60@gmail.com>
2023-05-11 22:46:57 +0200

    Print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _puts_recursion : prints a string, followed by a new line
+ * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
  */
 void _puts_recursion(char *s)
